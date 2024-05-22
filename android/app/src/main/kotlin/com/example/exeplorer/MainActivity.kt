13277678881fdbd95344cf3fd36cea00021083ca
canerkocak <canerotellie@gmail.com>
2024-05-22 23:50:08 +0200

    chore: clean up the flutter lmao, its a cool concept yes, but the support ICP has for flutter is the same amount of support 300 yr olden wooden bridge has, and im fat, that wooden bridge is gonna break, just like my mind from trying to understand how flutter works in combination with ICP, so real CHORE for tomorrow is, either react-native or kotlin to make the apps since i want it to be green text and blue text

--- android/app/src/main/kotlin/com/example/exeplorer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.exeplorer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/models/canister_response.dart
+++
@@ -1,5 +0,0 @@
-class CanisterResponse {
-  final BigInt balance;
-
-  CanisterResponse({required this.balance});
-}
