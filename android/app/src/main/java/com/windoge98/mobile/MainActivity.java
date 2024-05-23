4c04307da424fdfa6f4d7336621c82539aba79cb
canerkocak <canerotellie@gmail.com>
2024-05-23 13:13:21 +0200

    chore: add ios/android directories + capacitor config

---
+++ android/app/src/main/java/com/windoge98/mobile/MainActivity.java
@@ -0,0 +1,5 @@
+package com.windoge98.mobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.windoge98.mobile',
+  appName: 'exeplorer',
+  webDir: 'build'
+};
+
+export default config;
